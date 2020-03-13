2771a9fb531bf2e7f8e7b428b48354b1db4bdcf7
sirishajinka <53328878+sirishajinka@users.noreply.github.com>
2020-03-13 17:45:31 +0530

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,28 +1,17 @@
-window.onload = function(){
-    var calc = new Calculator();
-    calc.init('X','Y','Output');
-};
-class Calculator{
-    private x : HTMLInputElement;
-    private y : HTMLInputElement;
-    private output : HTMLSpanElement;
-
-    init(x: string, y: string,output: string){
-        this.x=<HTMLInputElement>document.getElementById(x);
-        this.y=<HTMLInputElement>document.getElementById(y);
-        this.output=<HTMLSpanElement>document.getElementById(output);
-        this.wireEvents();
-    }
-
-    wireEvents(){
-        document.getElementById('Add').addEventListener('click',event => {this.output.innerHTML = this.add(parseInt(this.x.value),parseInt(this.y.value)).toString()});
-        document.getElementById('Subtract').addEventListener('click',event => {this.output.innerHTML = this.subtract(parseInt(this.x.value),parseInt(this.y.value)).toString()});
-    }
-
-    add(x: number,y: number){
-        return x+y;
-    }
-    subtract(x: number,y: number){
-        return x-y;
+interface Ipoint{
+    getDist(): number;
+}
+module Shapes{
+    export class Point implements Ipoint{
+        constructor(public x: number, public y: number){}
+        getDist(){
+            return Math.sqrt(this.x * this.x + this.y * this.y);
+        }
+        static origin = new Point(0,0);
     }
 }
+
+var p: Ipoint = new Shapes.Point(3,4);
+var dist = p.getDist();
+console.log(dist);
+
